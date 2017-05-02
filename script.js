e31856c9e32353424f71f08bc4497de2cd094990
Kyle Hutchens <Hutch8729@gmail.com>
2017-05-02 14:06:12 -0400

    fully functional

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 var apiBaseURL = "https://wind-bow.gomix.me/twitch-api/";
 var channelsArr = ["NALCS1", "NALCS2", "riotgames", "freecodecamp", "summit1g", "imaqtpie", "ESL_CSGO", "noobs2ninjas", "comster404", "c9sneaky", "brunofin"];
-var doesNotExistArr = [];
-var existsArr = [];
-var onlineArr = [];
-var offlineArr = [];
 //var testerArr = ["comster404"];
 var testerArr = ["NALCS1"];
-
 
 function checkTwitchChannelExists(){
 
@@ -38,16 +33,44 @@ function checkTwitchChannelStreaming(user, channelData){
 }
 
 function make404Div(user, userData){
-	var searchResults = "<div class='channel no-channel' id='" + user + "'><i class='fa fa-ban float-left fa-5x' aria-hidden='true'></i><h2>" + user + "</h2><p>" + userData.message + "</p></div>";
+	var searchResults = "<div class='channel no-channel shown' id='" + user + "'><i class='fa fa-ban float-left fa-5x' aria-hidden='true'></i><h2>" + user + "</h2><p>" + userData.message + "</p></div>";
 	$('#doesNotExistHolder').append(searchResults);
 }
 
 function makeOnlineDiv(user, userData){
-	var searchResults = "<div class='channel online' id='" + user + "'><img src=" + userData.stream.channel.logo + " class='float-left channelLogo'><h2>" + user + "</h2><p><strong>" + userData.stream.game + "</strong> - " + userData.stream.channel.status + "</p></div>";
+	var searchResults = "<div class='channel online shown' id='" + user + "'><img src=" + userData.stream.channel.logo + " class='float-left channelLogo'><h2>" + user + "</h2><p><strong>" + userData.stream.game + "</strong> - " + userData.stream.channel.status + "</p></div>";
 	$('#onlineHolder').append(searchResults);
 }
 
 function makeOfflineDiv(user, userData, channelData){
-	var searchResults = "<div class='channel offline' id='" + user + "'><img src=" + channelData.logo + " class='float-left channelLogo'><h2>" + user + "</h2><p><strong>OFFLINE</strong></p></div>";
+	var searchResults = "<div class='channel offline shown' id='" + user + "'><img src=" + channelData.logo + " class='float-left channelLogo'><h2>" + user + "</h2><p><strong>OFFLINE</strong></p></div>";
 	$('#offlineHolder').append(searchResults);
 }
+
+function displayAll(){
+	$('.no-channel').addClass("shown").removeClass("hidden");
+	$('.online').addClass("shown").removeClass("hidden");
+	$('.offline').addClass("shown").removeClass("hidden");
+}
+
+function displayOnline(){
+	$('.no-channel').addClass("hidden").removeClass("shown");
+	$('.online').addClass("shown").removeClass("hidden");
+	$('.offline').addClass("hidden").removeClass("shown");
+}
+
+function displayOffline(){
+	$('.no-channel').addClass("hidden").removeClass("shown");
+	$('.online').addClass("hidden").removeClass("shown");
+	$('.offline').addClass("shown").removeClass("hidden");
+}
+
+$('#channelsHolder').on('click', '.channel', this.id, function(){
+	if($("#" + this.id).hasClass("online") || $("#" + this.id).hasClass("offline")){
+		window.location.href = "https://twitch.tv/" + this.id;
+	}
+});
+
+$(document).ready(function(){
+	checkTwitchChannelExists();
+});
